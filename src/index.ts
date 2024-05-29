291b7d68991a92f8b941cf930eabfd8c13896df3
szilardjanko <szilard.janko@gmail.com>
2024-05-29 19:57:45 -0400

    3d scene and menu ui

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
+import {
+  Billboard,
+  engine,
+  GltfContainer,
+  Material,
+  MaterialTransparencyMode,
+  MeshRenderer,
+  Transform
+} from '@dcl/sdk/ecs'
 import { setupUi } from './ui'
+import { Color4 } from '@dcl/sdk/math'
 
 export function main() {
-
   setupUi()
+
+  const scene = engine.addEntity()
+  GltfContainer.create(scene, {
+    src: 'assets/scene/models/bingoScene.glb'
+  })
+  Transform.create(scene, {
+    position: { x: 0, y: 0, z: 0 },
+    scale: { x: 1, y: 1, z: 1 }
+  })
+
+  const blueButtonGrid = engine.addEntity()
+  MeshRenderer.setPlane(blueButtonGrid)
+  Transform.create(blueButtonGrid, {
+    position: { x: 38.2, y: 7, z: 32.5 },
+    scale: { x: 7, y: 7, z: 1 }
+  })
+  Material.setPbrMaterial(blueButtonGrid, {
+    texture: Material.Texture.Common({
+      src: 'images/blueButtonGrid.png'
+    }),
+    transparencyMode: MaterialTransparencyMode.MTM_ALPHA_TEST,
+    alphaTest: 1,
+    metallic: 0.1,
+    albedoColor: Color4.White()
+  })
+  Billboard.create(blueButtonGrid, {})
 }
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,31 +1,365 @@
 import { Color4 } from '@dcl/sdk/math'
-import ReactEcs, { Label, ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
+import ReactEcs, { Label, Button, Input, ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
 
 export function setupUi() {
   ReactEcsRenderer.setUiRenderer(uiComponent)
 }
 
+let showMenu = true
+let showJoinGameMenu = false
+let showNewGameMenu = false
+let showRulesMenu = false
+let showLeaderboardsMenu = false
+
 const uiComponent = () => (
   <UiEntity
     uiTransform={{
-      width: 400,
-      height: 230,
-      margin: '16px 0 8px 270px',
-      padding: 4
+      display: 'flex',
+      positionType: 'absolute',
+      position: {
+        right: '0%',
+        top: '0%'
+      },
+      width: '75%',
+      height: '100%'
     }}
-    uiBackground={{ color: Color4.create(0.5, 0.8, 0.1, 0.6) }}
   >
     <UiEntity
       uiTransform={{
-        width: '100%',
-        height: '100%',
+        display: showMenu ? 'flex' : 'none',
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'space-between'
+        positionType: 'absolute',
+        position: {
+          right: '0%',
+          bottom: '0%'
+        },
+        width: 373.5,
+        height: 691.5
       }}
-      uiBackground={{ color: Color4.fromHexString('#70ac76ff') }}
+      uiBackground={{
+        textureMode: 'stretch',
+        texture: {
+          src: 'images/menu.png'
+        }
+      }}
     >
-      <Label value="Bingo" color={Color4.White()} fontSize={24} />
+      <Button
+        uiTransform={{
+          width: 279.75,
+          height: 61.5,
+          position: {
+            top: '44%'
+          }
+        }}
+        value=""
+        variant="secondary"
+        fontSize={18}
+        onMouseDown={() => {
+          showMenu = false
+          showJoinGameMenu = true
+        }}
+        uiBackground={{
+          textureMode: 'stretch',
+          texture: {
+            src: 'images/joinGame.png'
+          }
+        }}
+      />
+      <Button
+        uiTransform={{
+          width: 279.75,
+          height: 61.5,
+          position: {
+            top: '47%'
+          }
+        }}
+        value=""
+        variant="secondary"
+        fontSize={18}
+        onMouseDown={() => {
+          showMenu = false
+          showNewGameMenu = true
+        }}
+        uiBackground={{
+          textureMode: 'stretch',
+          texture: {
+            src: 'images/newGame.png'
+          }
+        }}
+      />
+      <Button
+        uiTransform={{
+          width: 279.75,
+          height: 61.5,
+          position: {
+            top: '50%'
+          }
+        }}
+        value=""
+        variant="secondary"
+        fontSize={18}
+        onMouseDown={() => {
+          showMenu = false
+          showRulesMenu = true
+        }}
+        uiBackground={{
+          textureMode: 'stretch',
+          texture: {
+            src: 'images/rules.png'
+          }
+        }}
+      />
+      <Button
+        uiTransform={{
+          width: 279.75,
+          height: 61.5,
+          position: {
+            top: '53%'
+          }
+        }}
+        value=""
+        variant="secondary"
+        fontSize={18}
+        onMouseDown={() => {
+          showMenu = false
+          showLeaderboardsMenu = true
+        }}
+        uiBackground={{
+          textureMode: 'stretch',
+          texture: {
+            src: 'images/leaderboards.png'
+          }
+        }}
+      />
+    </UiEntity>
+    <UiEntity // Join Game
+      uiTransform={{
+        display: showJoinGameMenu ? 'flex' : 'none',
+        flexDirection: 'column',
+        alignItems: 'center',
+        positionType: 'absolute',
+        position: {
+          right: '0%',
+          bottom: '0%'
+        },
+        width: 480,
+        height: 480
+      }}
+      uiBackground={{
+        textureMode: 'stretch',
+        texture: {
+          src: 'images/menuGames.png'
+        }
+      }}
+      onMouseDown={() => {
+        showMenu = true
+        showJoinGameMenu = false
+      }}
+    >
+      {[1, 2, 3, 4, 5].map((i) => (
+        <Label
+          key={i}
+          uiTransform={{
+            width: 279.75,
+            height: 61.5,
+            margin: {
+              top: i === 1 ? `10%` : `4%`
+            }
+          }}
+          uiBackground={{
+            textureMode: 'stretch',
+            texture: {
+              src: 'images/button.png'
+            }
+          }}
+          value={`Game ${i}`}
+          fontSize={24}
+          onMouseDown={() => {}}
+        />
+      ))}
+    </UiEntity>
+    <UiEntity // New Game
+      uiTransform={{
+        display: showNewGameMenu ? 'flex' : 'none',
+        flexDirection: 'column',
+        alignItems: 'center',
+        positionType: 'absolute',
+        position: {
+          right: '0%',
+          bottom: '0%'
+        },
+        width: 480,
+        height: 480
+      }}
+      uiBackground={{
+        textureMode: 'stretch',
+        texture: {
+          src: 'images/menuGames.png'
+        }
+      }}
+    >
+      <Label
+        uiTransform={{
+          width: 279.75,
+          height: 61.5,
+          margin: {
+            top: `10%`
+          }
+        }}
+        value=""
+        fontSize={24}
+        uiBackground={{
+          textureMode: 'stretch',
+          texture: {
+            src: 'images/enterGamesName.png'
+          }
+        }}
+      />
+      <Input
+        uiTransform={{
+          width: 279.75,
+          height: 61.5,
+          margin: {
+            top: `1%`
+          }
+        }}
+        color={Color4.White()}
+        placeholderColor={Color4.White()}
+        placeholder="Enter games name"
+        fontSize={24}
+        value=""
+        onChange={() => {}}
+      />
+      <Button
+        uiTransform={{
+          width: 279.75,
+          height: 61.5,
+          margin: {
+            top: `4%`
+          }
+        }}
+        uiBackground={{
+          textureMode: 'stretch',
+          texture: {
+            src: 'images/createGame.png'
+          }
+        }}
+        value=""
+        variant="secondary"
+        fontSize={24}
+        onMouseDown={() => {}}
+      />
+      <Button
+        uiTransform={{
+          width: 279.75,
+          height: 61.5,
+          margin: {
+            top: `4%`
+          }
+        }}
+        uiBackground={{
+          textureMode: 'stretch',
+          texture: {
+            src: 'images/back.png'
+          }
+        }}
+        value=""
+        variant="secondary"
+        fontSize={24}
+        onMouseDown={() => {
+          showMenu = true
+          showNewGameMenu = false
+        }}
+      />
+    </UiEntity>
+    <UiEntity // Rules
+      uiTransform={{
+        display: showRulesMenu ? 'flex' : 'none',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        positionType: 'absolute',
+        position: {
+          right: '10%',
+          bottom: '10%'
+        },
+        width: 1254.4,
+        height: 716.8
+      }}
+      uiBackground={{
+        textureMode: 'stretch',
+        texture: {
+          src: 'images/rulesBoard.png'
+        }
+      }}
+    >
+      <Button
+        uiTransform={{
+          width: 279.75,
+          height: 61.5,
+          position: {
+            bottom: '3.5%'
+          }
+        }}
+        uiBackground={{
+          textureMode: 'stretch',
+          texture: {
+            src: 'images/back.png'
+          }
+        }}
+        value=""
+        variant="secondary"
+        fontSize={24}
+        onMouseDown={() => {
+          showMenu = true
+          showRulesMenu = false
+        }}
+      />
+    </UiEntity>
+    <UiEntity // Leaderboards
+      uiTransform={{
+        display: showLeaderboardsMenu ? 'flex' : 'none',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        positionType: 'absolute',
+        position: {
+          right: '10%',
+          bottom: '10%'
+        },
+        width: 1254.4,
+        height: 716.8
+      }}
+      uiBackground={{
+        textureMode: 'stretch',
+        texture: {
+          src: 'images/leaderboardsBoard.png'
+        }
+      }}
+    >
+      <Button
+        uiTransform={{
+          width: 279.75,
+          height: 61.5,
+          position: {
+            bottom: '3.5%'
+          }
+        }}
+        uiBackground={{
+          textureMode: 'stretch',
+          texture: {
+            src: 'images/back.png'
+          }
+        }}
+        value=""
+        variant="secondary"
+        fontSize={24}
+        onMouseDown={() => {
+          showMenu = true
+          showLeaderboardsMenu = false
+        }}
+      />
     </UiEntity>
   </UiEntity>
 )
